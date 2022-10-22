65a60535dda182efdbe69b287e27c106aba2feb7
indriyanto Nugroho <indriyantongrhh@gmail.com>
2022-10-22 23:12:16 +0700

    add paginatiion tes

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1630,8 +1630,8 @@ class ApiController extends Controller
                             DB::raw('((gradding.jumlah_sbw - dry_kedua.jumlah_sbw) / (gradding.jumlah_sbw / 100)) as persentasi_susut')
                             )
                             ->orderBy('id', 'DESC')
-                            // ->paginate(600); // for pagination
-                            ->get();
+                            ->paginate(20); // for pagination
+                            // ->get();
 
             $sumjumlahKepingAwal = DB::table('transaksi_data_grading_akhir')
                             ->leftjoin('dry_kedua','dry_kedua.id' , '=',  'transaksi_data_grading_akhir.id_dry_kedua')
