ce74984994a6d31bbdf06389b3a492de955ec77e
indriyanto Nugroho <indriyantongrhh@gmail.com>
2022-10-23 13:42:53 +0700

    tesss

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1602,7 +1602,7 @@ class ApiController extends Controller
         $data = $request->get('data');
             $gradeakhir = DB::table('transaksi_data_grading_akhir')
                             ->leftjoin('packing', 'packing.grade_akhir_id', '=', 'transaksi_data_grading_akhir.id')
-                            ->leftjoin('streaming', 'streaming.kode_transaksi_grading', '=', 'transaksi_data_grading_akhir.kode_transaksi_grading')
+                            // ->leftjoin('streaming', 'streaming.kode_transaksi_grading', '=', 'transaksi_data_grading_akhir.kode_transaksi_grading')
                             ->leftjoin('dry_kedua','dry_kedua.id' , '=',  'transaksi_data_grading_akhir.id_dry_kedua')
                             ->leftjoin('mandor', 'mandor.id', '=', 'dry_kedua.mandor_id')
                             ->leftjoin('gradding', 'gradding.id', '=', 'mandor.gradding_id')
